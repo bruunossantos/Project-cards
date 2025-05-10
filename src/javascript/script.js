51086b96789df4f7ba2bd72ec9d121f115899d8c
Bruno Santos <bruuneles@gmail.com>
2025-05-10 19:07:22 -0300

    Add cards
    Adicionada a função de adicionar novos cards nas colunas

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -25,3 +25,83 @@ document.querySelectorAll('.kanban-cards').forEach(column => {
         e.currentTarget.appendChild(dragCard);
     })
 })
+
+/*Fazendo modal para adicionar novos card*/
+
+let currentButton = null;
+
+
+const openButtons = document.querySelectorAll('.add-card');
+
+openButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        currentButton = button; // <-- Aqui!
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+        modal.showModal();
+    });
+});
+
+
+const closeButtons = document.querySelectorAll('.close-modal');
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+        modal.close();
+    });
+});
+
+const modalForm = document.querySelector('#modal-1 form');
+
+modalForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const title = document.getElementById('card-title').value;
+    const description = document.getElementById('card-description').value;
+    const priority = document.getElementById('card-priority').value;
+
+    if (!title || !description || !priority) return;
+
+    const newCard = document.createElement('div');
+    newCard.classList.add('kanban-card');
+    newCard.setAttribute('draggable', 'true');
+
+    const badgeText = {
+        high: 'Alta Prioridade',
+        medium: 'Média Prioridade',
+        low: 'Baixa Prioridade'
+    };
+
+    newCard.innerHTML = `
+        <div class="badge ${priority}">
+            <span>${badgeText[priority]}</span>
+        </div>
+        <p class="card-title">${title}</p>
+        <div class="card-infos">
+            <div class="card-icons">
+                <p><i class="fa-regular fa-comment"> 0 </i></p>
+                <p><i class="fa-solid fa-paperclip"> 0 </i></p>
+            </div>
+            <div class="user">
+                <img src="src/images/avatar.webp" alt="Avatar">
+            </div>
+        </div>
+    `;
+
+    newCard.addEventListener('dragstart', e => {
+        e.currentTarget.classList.add('dragging');
+    });
+
+    newCard.addEventListener('dragend', e => {
+        e.currentTarget.classList.remove('dragging');
+    });
+
+    // Usando o botão clicado para encontrar a coluna correta
+    const column = currentButton.closest('.kanban-column').querySelector('.kanban-cards');
+    column.appendChild(newCard);
+
+    document.getElementById('modal-1').close();
+    modalForm.reset();
+});
